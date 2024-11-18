1a780b71acfa3f6ece97bd353058e595078f56f8
gambaLunatica <juanaluna553@gmail.com>
2024-11-18 14:27:57 +0100

    ahora si cambia la pag bien

---
+++ index1.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+    <center>
+        <img src="logolaliga.png" width="150">
+        <br>
+        <br>
+        <form action="identifica.php" method="post">
+            <table>
+                <tr>
+                    <td>Usuari: </td>
+                    <td><input name="user"></td>
+                </tr>
+                <tr>
+                    <td>Contrasenya</td>
+                    <td><input name="pass" type="password"></td>
+                </tr>
+            </table>
+            <br>
+            <br><input name="Login" type="submit">
+        </form>
+    </center>
+</body>
+</html>
